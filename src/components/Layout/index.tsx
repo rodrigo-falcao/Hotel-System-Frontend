49bcbed9eefd86c2ffd0f2c653dfade2a70297c0
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-08-08 12:52:35 -0300

    Feat: Create basic layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,19 @@
+type LayoutProps = {
+    children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <>
+            <header className="w-full flex justify-center py-5 border-b border-light-grey-400">
+                Logo DNC
+            </header>
+            <main className="w-full flex justify-center align-middle">{children}</main>
+            <footer className="w-full flex justify-center py-6 bg-snow-white border-t border-light-grey-500">
+                <p>&copy; 2025 Hotel System. All rights reserved.</p>
+            </footer>
+        </>
+    );
+};
+
+export default Layout;
