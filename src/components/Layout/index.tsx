c4e9bbf45b2f9d60954d60040b46f2a8225b9ab4
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-08-09 11:19:47 -0300

    Feat: Update Image logo

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type LayoutProps = {
     children: React.ReactNode;
 };
@@ -6,7 +8,7 @@ const Layout = ({ children }: LayoutProps) => {
     return (
         <>
             <header className="w-full flex justify-center py-5 border-b border-light-grey-400">
-                Logo DNC
+                <Image src="/logo.svg" alt="Logo DNC" width={45} height={40} />
             </header>
             <main className="w-full flex justify-center align-middle">{children}</main>
             <footer className="w-full flex justify-center py-6 bg-snow-white border-t border-light-grey-500">
