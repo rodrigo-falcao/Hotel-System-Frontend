a6ce768fd3b0fdf569bdd2cf5159eb96089b3d2e
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-08-10 16:38:08 -0300

    Feat:Create Alert for User

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import ImageField from "@/components/form/ImageField";
-import TextField from "@/components/form/TextField";
+import Alert from "@/components/Alert";
 import CustomLink from "@/components/Link";
+import ImageField from "@/components/form/ImageField";
 import Pagination from "@/components/Pagination";
+import TextField from "@/components/form/TextField";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <TextField label='Nome Completo' id="full_name" name="full_name" error="Campo obrigatório" />
       <ImageField label="Selecione a foto" id="profile_picture" name="profile_picture" />
       <Pagination currentPage={100} totalPages={100} destination="/" />
+      <Alert type="success">Esse é um feedback para o meu usuário</Alert>
+      <Alert type="danger">Esse é um feedback de erro para o meu usuário</Alert>
     </section>
   );
 }
